beb45222328f9d7bd1eb305356c8f2cfeb94dc17
Bruksin <smirnov.pa@phystech.edu>
2022-04-30 02:39:37 +0300

    Base server

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-pass
+import socket
+
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_socket.bind(('', 53210))
+serv_socket.listen(10)
+
+while True:
+    client_socket, client_address = serv_socket.accept()
+    print('Connected by', client_address)
+
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        client_socket.sendall(data)
+
+    client_socket.close()
