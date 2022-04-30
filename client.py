1313596cd94b2e8cc0ea7a259b3e9e785cdb19b3
Bruksin <smirnov.pa@phystech.edu>
2022-04-30 03:23:11 +0300

    Simple working example

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('127.0.0.1', 53210))
-client_socket.sendall(b'Hello, world')
+# client_socket.sendall(b'Hello, world')
 data = client_socket.recv(1024)
 client_socket.close()
-print('Received', repr(data))
+print(data.decode())
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import errno
+import time
 
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 serv_socket.bind(('', 53210))
@@ -8,10 +10,10 @@ while True:
     client_socket, client_address = serv_socket.accept()
     print('Connected by', client_address)
 
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        client_socket.sendall(data)
+    time.sleep(1)
+    # data = client_socket.recv(1024)
+    data = ('Your address is: ' + str(client_address[1])).encode()
+
+    client_socket.sendall(data)
 
     client_socket.close()
